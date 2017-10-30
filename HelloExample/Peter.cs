f3156c6c67762cfa27954f895ed1ce4b83223701
novak vova <novakvova@gmail.com>
2017-10-30 15:45:59 +0200

    Begin Work Peter

--- HelloExample/Peter.cs
+++ HelloExample/Peter.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace HelloExample
 {
-    class Program
+    public class Peter
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Peter");
-        }
     }
 }
